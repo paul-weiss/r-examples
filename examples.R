ea06d5ef88804293a6d2b295a90c67171e47982d
paul <paulweiss@protonmail.com>
2024-03-16 14:07:01 -0400

    update

---
+++ examples.R
@@ -0,0 +1,38 @@
+# examples.R
+#------------------------------------------------------------
+# Add libraries here and it will install/load from vector
+#------------------------------------------------------------
+libs = c(
+  "lobstr"
+)
+
+for (i in libs) {
+  if (!is.element(i, .packages(all.available = TRUE))) {
+    install.packages(i, repos = "http://cran.us.r-project.org")
+  }
+  library(i, character.only = TRUE)
+}
+
+# Constants:
+#--------------------------
+
+same_object <- function(x, y) {
+  if(obj_addr(x) == obj_addr(y)) {
+    message(paste("x and y have same addresses = ", obj_addr(x)))
+  } else {
+    message(paste("x and y have different addresses (", obj_addr(x), ",", obj_addr(y), sep=""))
+  }
+}
+
+
+x <- c(1, 2, 3)
+y <- x
+
+same_object(x, y)
+
+a <- 1:10
+b <- a
+c <- b
+d <- 1:10
+
+same_object(a,d)
